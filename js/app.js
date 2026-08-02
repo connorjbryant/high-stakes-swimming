489943d3ff541479c68a67a2be56e9b6c78515db
Connor Bryant <connorjamesbryant7@gmail.com>
2026-08-01 20:51:04 -0400

    3D update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,71 @@
 jQuery(function($){
 
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer({ canvas: document.getElementById("three-canvas"), antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Camera pos for hallways
+    camera.position.set(0, 2, 8);
+    camera.lookAt(0, 0, -5);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+    scene.add(ambientLight);
+
+    const pointLight = new THREE.PointLight(0xffffff, 1.5, 30);
+    pointLight.position.set(0, 3, 5);
+    scene.add(pointLight);
+
+    let targetCameraZ = 8;
+
+    let visualCorridors = [];
+
+    function animate(){
+        requestAnimationFrame(animate);
+        camera.position.z += (targetCameraZ - camera.position.z) * 0.1;
+        renderer.render(scene, camera);
+    }
+    animate();
+
+    // 3D translator function
+    function draw3DCorridors(){
+        visualCorridors.forEach(box => scene.remove(box));
+        visualCorridors = [];
+
+        // Grab all list elements from game loop
+        const totalOptions = $(rabbithole).find("li");
+
+        totalOptions.each(function(index){
+            const spacing = 5;
+            const xPosition = (index - (totalOptions.length - 1) / 2) * spacing;
+
+            // Basic wireframe hallway box
+            const geometry = new THREE.BoxGeometry(4, 4, 15);
+
+            // Mechanic visual
+            const isCheckpoint = ($(this).attr("id") === "checkpoint");
+
+            const boxColor = isCheckpoint ? 0x00ffcc : 0xff00ff;
+
+            const material = new THREE.MeshStandardMaterial({
+                color: boxColor,
+                roughness: 0.4,
+                metalness: 0.2,
+                side: THREE.BackSide
+            });
+            const boxMesh = new THREE.Mesh(geometry, material);
+
+            const edges = new THREE.EdgesGeometry(geometry);
+            const lineMat = new THREE.LineBasicMaterial({color: isCheckpoint ? 0x00ffcc : 0xff00ff });
+            const line = new THREE.LineSegments(edges, lineMat);
+            boxMesh.add(line);
+
+            boxMesh.position.set(xPosition, 0, -7.5);
+            scene.add(boxMesh);
+            visualCorridors.push(boxMesh);
+        })
+    }
+
     var player = $(".js-player");
     var friend = $(".js-friend");
     var rabbithole = $("#game");
@@ -8,9 +74,11 @@ jQuery(function($){
     var autoMoveTimer = null;
     var moveDirection = 1;
     // Random num generation 1-10 for random list amounts
-    var randomNum = Math.floor(Math.random() * 6) + 2;
+    var randomNum = Math.floor(Math.random() * 4) + 2;
 
-    $("#startBtn").on("click", function(){
+    $("#startBtn").on("click", function(e){
+        e.stopPropagation();
+        $(rabbithole).empty();
         for (var i = 0; i <= randomNum; i++){
             $(rabbithole).append("<li></li>");
         }
@@ -25,7 +93,7 @@ jQuery(function($){
         allLis.eq(randomIndex).append(friend).attr("id", "checkpoint");
 
         $(this).hide();
-
+        draw3DCorridors();
         startAutoMovement();
     });
 
@@ -98,6 +166,14 @@ jQuery(function($){
 
             var targetTile = allTiles.eq(targetIndex);
             player.appendTo(targetTile);
+
+            visualCorridors.forEach((box, idx) => {
+                if (idx === targetIndex){
+                    box.scale.set(1.1, 1.1, 1);
+                } else {
+                    box.scale.set(1, 1, 1);
+                }
+            });
         }, 400);
         
     }
@@ -109,24 +185,26 @@ jQuery(function($){
         }
     });
 
-    $(document).on("click", function (event) {
-        if ($(event.target).is("#startBtn")) {
-            return;
-        }
-
+    $("#three-canvas").on("click", function(event){
         attemptAdvance();
-    });
+    })
 
     function attemptAdvance(){
-        var playerTile = player.parent()[0];
-        var friendTile = friend.parent()[0];
+        var playerTile = player.parent();
+        var friendTile = friend.parent();
 
-        if (playerTile === friendTile){
+        if (playerTile.length && friendTile.length && playerTile.is(friendTile)){
             console.log("correct");
 
             //var activeTile = $(".checkpoint");
             var activeTile = $("#checkpoint");
-            spawnPaths(activeTile);
+
+            targetCameraZ = -5;
+
+            setTimeout(function(){
+                spawnPaths(activeTile);
+                targetCameraZ = 8;
+            }, 400);
         } else {
             console.log("wrong");
             gameOver();
@@ -144,26 +222,21 @@ jQuery(function($){
     // }
 
     function spawnPaths(activeTile){
-        //activeTile.removeClass("checkpoint");
         activeTile.removeAttr("id");
-        var freshRandomNum = Math.floor(Math.random() * 6) + 2;
-        var newUl = $("<ul></ul>");
+
+        $(rabbithole).empty();
+
+        var freshRandomNum = Math.floor(Math.random() * 4) + 2;
+
         for (var i = 0; i <= freshRandomNum; i++){
-            newUl.append("<li></li>");
+            $(rabbithole).append("<li></li>");
         }
-        activeTile.append(newUl);
-        // Isolate the choices to the new paths
-        var newLis = newUl.find("li");
 
-        // Pick a random index based on the num of paths for the friend again
+        var newLis = $(rabbithole).find("> li");
         var randomIndex = Math.floor(Math.random() * newLis.length);
-
-        //newLis.eq(randomIndex).append(friend).addClass("#checkpoint");
         newLis.eq(randomIndex).append(friend).attr("id", "checkpoint");
 
-        $("html, body").animate({
-            scrollTop: newLis.eq(randomIndex).offset().top - ($(window).height() / 2)
-        }, 500);
+        draw3DCorridors();
     }
 
     function gameOver(){
@@ -173,5 +246,11 @@ jQuery(function($){
         $("#startBtn").show().text("Try Again?");
         location.reload();
     }
+
+    $(window).on("resize", function(){
+        camera.aspect = $(window).width() / $(window).height();
+        camera.updateProjectionMatrix();
+        renderer.setSize($(window).width(), $(window).height());
+    })
 
 });
